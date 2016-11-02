79badab3fd13566a0f75c5ed1b6b0e7450e53b8a
fengmk2 <m@fengmk2.com>
2016-11-02 17:05:06 +0800

    feat: add ENV
    https://github.com/cnpm/npminstall/issues/157

--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,7 @@
-/**
- * Copyright(c) cnpm and other contributors.
- * MIT Licensed
- *
- * Authors:
- *   fengmk2 <m@fengmk2.com> (http://fengmk2.com)
- */
-
 'use strict';
 
-/**
- * Module dependencies.
- */
-
 const assert = require('assert');
-const config = require('../');
+const config = require('..');
 
 describe('test/index.test.js', () => {
   it('should config exists', () => {
@@ -22,5 +10,6 @@ describe('test/index.test.js', () => {
     assert.equal(config.china.sqlite3.host, 'https://npm.taobao.org/mirrors');
     assert.equal(config.china.fsevents.host, 'https://npm.taobao.org/mirrors/fsevents');
     assert.equal(config.china['flow-bin'].host, 'https://npm.taobao.org/mirrors/flow/v');
+    assert.equal(config.china.ENVS['CHROMEDRIVER_CDNURL'], 'https://tnpm-hz.oss-cn-hangzhou.aliyuncs.com/dist/chromedriver');
   });
 });
