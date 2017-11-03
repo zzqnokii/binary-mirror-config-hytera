50086339b1e51cda65b0210f1b2f177ec409b6cb
fengmk2 <fengmk2@gmail.com>
2017-11-03 15:09:21 +0800

    feat: add PUPPETEER_DOWNLOAD_HOST=https://storage.googleapis.com.cnpmjs.org (#2)
    closes https://github.com/cnpm/cnpmjs.org/issues/1246

--- test/index.test.js
+++ test/index.test.js
@@ -6,10 +6,10 @@ const config = require('..');
 describe('test/index.test.js', () => {
   it('should config exists', () => {
     console.log(config);
-    assert.deepEqual(Object.keys(config), ['china']);
+    assert.deepEqual(Object.keys(config), [ 'china' ]);
     assert.equal(config.china.sqlite3.host, 'https://npm.taobao.org/mirrors');
     assert.equal(config.china.fsevents.host, 'https://npm.taobao.org/mirrors/fsevents');
     assert.equal(config.china['flow-bin'].host, 'https://npm.taobao.org/mirrors/flow/v');
-    assert.equal(config.china.ENVS['CHROMEDRIVER_CDNURL'], 'https://tnpm-hz.oss-cn-hangzhou.aliyuncs.com/dist/chromedriver');
+    assert.equal(config.china.ENVS.CHROMEDRIVER_CDNURL, 'https://tnpm-hz.oss-cn-hangzhou.aliyuncs.com/dist/chromedriver');
   });
 });
