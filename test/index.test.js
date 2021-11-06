9e1e723c2c852864aafa746f5445dd9c83f03062
fengmk2 <fengmk2@gmail.com>
2021-11-07 00:48:36 +0800

    feat: change domain to npmmirror.com (#20)

--- test/index.test.js
+++ test/index.test.js
@@ -7,9 +7,9 @@ describe('test/index.test.js', () => {
   it('should config exists', () => {
     console.log(config);
     assert.deepEqual(Object.keys(config), [ 'china' ]);
-    assert.equal(config.china.sqlite3.host, 'https://cdn.npm.taobao.org/dist');
-    assert.equal(config.china.fsevents.host, 'https://cdn.npm.taobao.org/dist/fsevents');
-    assert.equal(config.china['flow-bin'].host, 'https://cdn.npm.taobao.org/dist/flow/v');
-    assert.equal(config.china.ENVS.CHROMEDRIVER_CDNURL, 'https://cdn.npm.taobao.org/dist/chromedriver');
+    assert.equal(config.china.sqlite3.host, 'https://oss.npmmirror.com/dist');
+    assert.equal(config.china.fsevents.host, 'https://oss.npmmirror.com/dist/fsevents');
+    assert.equal(config.china['flow-bin'].host, 'https://oss.npmmirror.com/dist/flow/v');
+    assert.equal(config.china.ENVS.CHROMEDRIVER_CDNURL, 'https://oss.npmmirror.com/dist/chromedriver');
   });
 });
