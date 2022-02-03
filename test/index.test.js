e69cc4650c7e3a5d43fa6e33dbbdaa8a502c0d87
fengmk2 <fengmk2@gmail.com>
2022-02-03 15:32:44 +0800

    feat: use the latest mirror cdn url (#25)

--- test/index.test.js
+++ test/index.test.js
@@ -7,9 +7,9 @@ describe('test/index.test.js', () => {
   it('should config exists', () => {
     console.log(config);
     assert.deepEqual(Object.keys(config), [ 'china' ]);
-    assert.equal(config.china.sqlite3.host, 'https://oss.npmmirror.com/dist');
-    assert.equal(config.china.fsevents.host, 'https://oss.npmmirror.com/dist/fsevents');
-    assert.equal(config.china['flow-bin'].host, 'https://oss.npmmirror.com/dist/flow/v');
-    assert.equal(config.china.ENVS.CHROMEDRIVER_CDNURL, 'https://oss.npmmirror.com/dist/chromedriver');
+    assert.equal(config.china.sqlite3.host, 'https://cdn.npmmirror.com/binaries');
+    assert.equal(config.china.fsevents.host, 'https://cdn.npmmirror.com/binaries/fsevents');
+    assert.equal(config.china['flow-bin'].host, 'https://cdn.npmmirror.com/binaries/flow/v');
+    assert.equal(config.china.ENVS.CHROMEDRIVER_CDNURL, 'https://cdn.npmmirror.com/binaries/chromedriver');
   });
 });
