d229280eb35db1c075d672b63dd604677e43a6d4
fengmk2 <fengmk2@gmail.com>
2022-04-14 15:51:43 +0800

    🐛 FIX: Use new sqlite3 mirror config (#28)
    https://github.com/TryGhost/node-sqlite3/commit/7f744a1dca2664af2fb8957ca68977ba36f134cb
    
    https://github.com/cnpm/cnpmcore/pull/208

--- test/index.test.js
+++ test/index.test.js
@@ -7,7 +7,7 @@ describe('test/index.test.js', () => {
   it('should config exists', () => {
     console.log(config);
     assert.deepEqual(Object.keys(config), [ 'china' ]);
-    assert.equal(config.china.sqlite3.host, 'https://cdn.npmmirror.com/binaries');
+    assert.equal(config.china.sqlite3.host, 'https://cdn.npmmirror.com/binaries/sqlite3');
     assert.equal(config.china.fsevents.host, 'https://cdn.npmmirror.com/binaries/fsevents');
     assert.equal(config.china['flow-bin'].host, 'https://cdn.npmmirror.com/binaries/flow/v');
     assert.equal(config.china.ENVS.CHROMEDRIVER_CDNURL, 'https://cdn.npmmirror.com/binaries/chromedriver');
